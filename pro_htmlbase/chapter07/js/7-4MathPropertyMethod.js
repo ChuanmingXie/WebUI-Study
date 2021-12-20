93ed6c528a366f1cb2c90d726bffdd35edc29f7c
ChuanmingXie <ChuanmingXie@outlook.com>
2021-12-20 10:32:38 +0800

    Js对象：Date对象和Math对象的示例

---
+++ pro_htmlbase/chapter07/js/7-4MathPropertyMethod.js
@@ -0,0 +1,57 @@
+/* Math对象属性的使用 */
+(function() {
+	document.write('<br />Math.PI=' + Math.PI);
+	document.write('<br />Math.E=' + Math.E);
+	document.write('<br />Math.LN2=' + Math.LN2);
+	document.write('<br />Math.LN10=' + Math.LN10);
+	document.write('<br />Math.LOG2E=' + Math.LOG2E);
+	document.write('<br />Math.LOG10E=' + Math.LOG10E);
+	document.write('<br />半径为4的圆的面积' + circularArea(4));
+	document.write("<br />半径为8，高为10的圆柱体的体积" + CylinderCapacity(8, 10));
+})()
+
+function circularArea(radius) {
+	return Math.PI * radius * radius;
+}
+
+function CylinderCapacity(radius, height) {
+	return Math.PI * radius * radius * height;
+}
+
+
+/* Math 对象方法的使用 */
+(function() {
+	maxAndMin();
+	var randomNum = 60 + Math.random() * 40;
+	document.write("随机生成一个60-100之间的数：" + randomNum);
+	document.write("<br />round()四舍五入的结果" + Math.round(randomNum));
+	document.write("<br />ceil()向上取整的结果：" + Math.ceil(randomNum));
+	document.write("<br />向下取整的结果：" + Math.floor(randomNum));
+	document.write("<hr />");
+	document.write("2的3次幂：" + Math.pow(2, 3));
+	document.write("<br />16的平方根：" + Math.sqrt(16));
+	document.write("<br />90<sup>o</sup>的正弦值是：" + Math.sin(90 * Math.PI / 180));
+})()
+
+function maxAndMin() {
+	var num = 10;
+	document.write("<br/><br/><hr />随机生成" + num + "个整数:");
+	var array = [];
+	var maxNum = 0;
+	var minNum = 0;
+	for (var i = 0; i < num; i++) {
+		var tmp = Math.random() * 100;
+		array[i] = Math.floor(tmp);
+		document.write(array[i] + "\t");
+		if (i == 0) {
+			maxNum = array[0];
+			minNum = array[0];
+		} else {
+			maxNum = Math.max(maxNum, array[i]);
+			minNum = Math.min(minNum, array[i]);
+		}
+	}
+	document.write("<br />随机数中，最大数为：" + maxNum);
+	document.write("<br />随机数中，最小数为：" + minNum);
+	document.write("<hr />")
+}
---
+++ pro_htmlbase/chapter07/js/7-4dateMethod.js
@@ -0,0 +1,46 @@
+var filePrefix = "<img src='img/Number_";
+var fileSuffix = ".png'/>";
+
+(function() {
+	var myDate = new Date();
+	showPicTime(myDate);
+
+	myDate.setFullYear(2045, 2, 23);
+	myDate.setHours(11, 23, 54);
+	showPicTime(myDate);
+
+	myDate.setDate(myDate.getDate() + 5);
+	showPicTime(myDate);
+})()
+
+function showPicTime(myDate) {
+	var year = myDate.getFullYear();
+	var month = myDate.getMonth() + 1;
+	var date = myDate.getDate();
+	var hour = myDate.getHours();
+	var minute = myDate.getMinutes();
+	var second = myDate.getSeconds();
+	document.write(numImage(year) + "<img src='img/year.jpg' />" +"<span>年</span>"+
+		numImage(fixTime(month)) + "<img src='img/month.jpg' />" +"<span>月</span>"+
+		numImage(fixTime(date)) + "<img src='img/day.jpg' />" +"<span>日</span>"+
+		numImage(fixTime(hour)) + "<img src='img/hour.jpg' />" +"<span>时</span>"+
+		numImage(fixTime(minute)) + "<img src='img/minute.jpg' />" +"<span>分</span>"+
+		numImage(fixTime(second)) + "<img src='img/second.jpg' />" + "<span>秒</span>"+ "<br/>")
+}
+
+function numImage(num) {
+	var str = num.toString();
+	var result = "";
+	for (var i = 0; i < str.length; i++) {
+		var c = "00"+str.charAt(i);
+		result += filePrefix + c + fileSuffix;
+	}
+	return result;
+}
+
+function fixTime(time) {
+	if (time < 10) {
+		return "0" + time;
+	}
+	return time;
+}
