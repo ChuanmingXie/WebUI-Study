e09b3d10af1a3ad6c65f6aed3954b22024c4f856
ChuanmingXie <ChuanmingXie@outlook.com>
2021-12-24 19:35:04 +0800

    10-14多媒体播放-H5多媒体API-自定义视频播放器

--- pro_htmlbase/chapter10/js/10-14videoPlayer.js
+++ pro_htmlbase/chapter10/js/10-14videoPlayer.js
@@ -1,9 +1,12 @@
+/* 10-14多媒体播放-H5多媒体API-自定义视频播放器 */
+
 var myVideo=$('#myVideo').get(0);
 var videoPlayer=$('#videoPlayer').get(0);
 var videoRange=$('#videoRange').get(0);
 var videoVoice=$('#videoVoice').get(0);
 var videoInfo=$('#videoInfo').get(0);
 
+/* 暂停与播放切换 */
 videoPlayer.onclick=function(){
 	if(myVideo.paused){
 		myVideo.play();
@@ -14,6 +17,7 @@ videoPlayer.onclick=function(){
 	}
 };
 
+/* 视频播放时，滚动条同步 */
 myVideo.ontimeupdate=function(){
 	var currentTime=Math.round(myVideo.currentTime*Math.pow(10,2)/Math.pow(10,2));
 	var totalTime=Math.round(myVideo.duration*Math.pow(10,2)/Math.pow(10,2));
@@ -24,15 +28,18 @@ myVideo.ontimeupdate=function(){
 	}
 };
 
+/* 拖动滚动条，视频进度同步 */
 videoRange.onmousedown=function(){
 	myVideo.pause();
 };
 
+/* 播放 */
 videoRange.onmouseup=function(){
 	myVideo.currentTime=myVideo.duration*(videoRange.value/videoRange.max);
 	myVideo.play();
 }
 
+/* 静音与取消静音切换 */
 videoVoice.onclick=function(){
 	if(!myVideo.muted){
 		videoVoice.value="取消静音";
