913a0d9e6f118373d7fd38b97bb75dc66fc52b8c
ChuanmingXie <ChuanmingXie@outlook.com>
2021-12-25 09:07:03 +0800

    Update 7-1arrayPropertyMethod.js

--- pro_htmlbase/chapter07/js/7-1arrayPropertyMethod.js
+++ pro_htmlbase/chapter07/js/7-1arrayPropertyMethod.js
@@ -79,15 +79,20 @@ function arrayMehod2() {
 
 	showFoodsByJoin(foods);
 
-	foods.push("淄博烧饼");
+	foods.unshift("淄博烧饼");
 
 	showFoodsByJoin(foods);
-
-	foods.splice(2, 2, "北京烤鸭");
+	/*
+	向数组中添加1-n个元素或者从数组中删除元素，
+	index必选，规定添加和删除元素的位置，index<0 时从数组尾部向前计数,-1表示最后一个元素，-2表示导数第二
+	howmany必选，表示要删除元素的数量，0代表不删除数据
+	[items]可选：表示向数组中添加和替换的元素
+	*/
+	foods.splice(2, 2, "北京烤鸭");	//从第二项开始删除两项，替换为“北京烤鸭”,总数减少一项
 
 	showFoodsByJoin(foods);
-
-	var betterFood = foods.slice(1, 4);
+	/* 从数组中返回指定的元素 */
+	var betterFood = foods.slice(1, 4);	//
 	document.write("<hr>获赞的美食：" + betterFood);
 	document.write("<hr>获赞的美食反向显示：" + betterFood.reverse());
 
@@ -102,7 +107,7 @@ function matrixArray() {
 	var title = new Array("电影类型", "电影名称", "票价");
 	var movies = [
 		["科幻", "流浪地球", 57.9],
-		["动漫", "巫山五行", 27.9],
+		["动漫", "雾山五行", 27.9],
 		["战争", "长津湖", 87.9],
 		["历史", "我和我的父辈", 97.9],
 	];
