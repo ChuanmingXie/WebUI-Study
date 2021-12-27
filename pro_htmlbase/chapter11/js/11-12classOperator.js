c53274dfca693f6e4f215a8709b4d432a34f0655
ChuanmingXie <ChuanmingXie@outlook.com>
2021-12-27 22:07:23 +0800

    11-12jQuery基本操作-样式操作-addClass|removeClass|toggleClass|css

--- pro_htmlbase/chapter11/js/11-12classOperator.js
+++ pro_htmlbase/chapter11/js/11-12classOperator.js
@@ -1,15 +1,49 @@
 /* 11-12jQuery基本操作-样式操作 */
-$(function(e){
-	$('#articleTitle').addClass('titleClass');		//添加某种样式
-	$("p[title='desc']").addClass("baseClass fontColor");		//添加多种样式
+/* addClass() | removeClass() */
+$(function(e) {
+	$('#articleTitle').addClass('titleClass'); //添加某种样式
+	$("p[title='desc']").addClass("baseClass fontColor"); //添加多种样式
 	//根据位置的不同，添加不同的样式，index为索引，oldClass为原来样式
-	$("p[title='desc']").addClass(function(index,oldClass){
-		console.log(index+oldClass);
-		return 'p'+index;
+	$("p[title='desc']").addClass(function(index, oldClass) {
+		console.log(index + oldClass);
+		return 'p' + index;
 	});
 });
 
-function removeClass(){
-	$('p').removeClass("fontSize");	//移出指定的样式
-	$('p').removeClass();			//移出所有的样式
+function removeClass() {
+	$('p').removeClass("fontSize"); //移出指定的样式
+	$('p').removeClass(); //移出所有的样式
+}
+
+/* toggle() | css() */
+var count = 0;
+//获取焦点
+function userNameOnFocus() {
+	$('#userName').toggleClass('focusClass');
+}
+//部门改变
+function changeDept() {
+	$('#saleDept').toggleClass("inverseColor", ++count % 3 == 0);
+}
+//图片 放大
+function enlargeImage() {
+	$('img').css({
+		width: function(index, value) {
+			return parseFloat(value) * 1.1;
+		},
+		height: function(index, value) {
+			return parseFloat(value) * 1.1;
+		}
+	});
+}
+//图片 缩小
+function lessenImage() {
+	$('img').css({
+		width: function(index, value) {
+			return parseFloat(value) * 0.9;
+		},
+		height: function(index, value) {
+			return parseFloat(value) * 0.9;
+		}
+	})
 }
