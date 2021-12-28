8ae1b731215a3080db5aa41b417f04a25ea49e53
ChuanmingXie <ChuanmingXie@outlook.com>
2021-12-28 18:40:07 +0800

    11-6jQuery事件处理-事件绑定-on|hover

--- pro_htmlbase/chapter11/js/11-16eventBind.js
+++ pro_htmlbase/chapter11/js/11-16eventBind.js
@@ -1,5 +1,6 @@
 /* 11-16jQuery事件处理-事件绑定-bind|one|toggle|live|delegate|on|hover */
 $(function(){
+	
 	/* bind() 方法的使用 */
 	//bind(type,fn1):绑定一个点击事件
 	$('#bindBtn').bind('click',function(){
@@ -43,4 +44,48 @@ $(function(){
 			$('#toggleAnimateBtn').css('background-color','red');
 		});
 	})
+	
+	/* live() 方法*/
+	$('.eventBind2').live('click',function(){
+		alert($(this).html());
+	});
+	
+	/* 通过新增div代码测试 */
+	$('.eventBind2').append("<div id='rightDiv'>通过代码新增的Div</div>");
+	
+	/* delegate() 方法：对对象内符合条件的子元素绑定事件*/
+	$('span').delegate('button','click',function(){
+		//div的显示与隐藏
+		$('div').slideToggle();	
+	})
+	
+	/* on()方法 */
+	/* 绑定事件,代替bind()方法 */
+	$('.eventBind3 #leftDiv').on('click','#bindBtn',function(){
+		alert('使用bind()方法绑定事件处理');
+	});
+	
+	/* 绑定事件，并额外传递数据，代替degelate() */
+	$('.eventBind3').on('click','#dataBindBtn',{msg:'额外传递数据'},function(e){
+		alert(e.data.msg);
+	});
+		
+	//绑定事件,替代live() 方法
+	$('.eventBind3').on('click',"div[id!=leftDiv]",function(){
+		alert($(this).html());
+	});
+	
+	/* hover()方法 */
+	//绑定事件 hover() 方法
+	
+	$('.eventBind3 #rightDiv').hover(
+	function(){
+		var r=parseInt(Math.random()*255);
+		var g=parseInt(Math.random()*255);
+		var b=parseInt(Math.random()*255);
+		var rgb='rgb('+r+','+g+','+b+')';
+		$(this).css('background-color',rgb);
+	},function(){
+		$(this).css('background-color','white');
+	});
 })
