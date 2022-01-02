127ddae96ca85b4d13ad85f07a40c0e66a38175b
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-02 22:56:19 +0800

    后台管理系统-左侧菜单栏管理

---
+++ pro_htmlbase/Program/fashionShop/backend/js/11-04-02event-bind-menu.js
@@ -0,0 +1,17 @@
+/* 菜单栏导航切换 */
+$(function(){
+	$('.menuson li').click(function(){
+		$('.menuson li.active').removeClass('active')
+		$(this).addClass('active');
+	});
+	
+	$('.title').click(function(){
+		var $ul=$(this).next('ul');
+		$('dd').find('ul').slideUp();
+		if($ul.is(':visible')){
+			$(this).next('ul').slideUp();
+		}else{
+			$(this).next('ul').slideDown();
+		}
+	});
+})
