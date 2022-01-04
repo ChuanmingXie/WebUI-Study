2754d07fe8daf2792226263c053f918884ed38d5
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-04 09:08:00 +0800

    后台管理系统-添加页面-表单验证

--- pro_htmlbase/Program/fashionShop/backend/js/11-04-02event-bind-add.js
+++ pro_htmlbase/Program/fashionShop/backend/js/11-04-02event-bind-add.js
@@ -1,7 +1,7 @@
 /* 添加界面表单验证 */
 $(function() {
 	// 为商品名称输入框绑定获取焦点和失去焦点的处理函数
-	$("goodsName").on('focus', function() {
+	$("#goodName").on('focus', function() {
 		$(this).css({
 			backgroundColor: "#FFEC8B",
 			borderColor: "#999",
@@ -22,26 +22,30 @@ $(function() {
 	});
 
 	/* 为单价和团购价绑定获得焦点是的函数 */
-	$("inpur[id$='Price']").on('focus', function() {
+	$("input[id$='Price']").on('focus', function() {
 		$(this).css({
 			fontSize: '16px',
 			color: 'red'
 		});
 	});
 	/* 为文本框绑定失去焦点时的处理函数 */
-	$("input[type='number']:last").on('focus', function() {
+	$("input[type='number']:last").on('blur', function() {
 		if ($(this).val() < 0) {
-			alert('数量不能小于0');
-			$(this).select();
+			$(this).val=0;
+			confirm('数量不能小于0');
+			// $(this).select();
+			// return false;
 		}
 	});
-	
+
 	/* 验证时间格式处理函数 */
 	$("input[type='datetime-local']").on("blur", function() {
 		var dateReg = /(\d{4})[-\/](\d{1,2})[-\/](\d{1,2})[ ](\d{1,2})[:](\d{1,2})/;
 		if (!dateReg.test($(this).val())) {
-			alert('日期格式不正确，请查验格式!');
-			$(this).select();
+			$(this).val()="";
+			confirm('日期格式不正确，请查验格式!');
+			// $(this).select();
+			// return false;
 		}
 	});
 	/* 为发布商品按钮绑定处理函数 */
