0027b536fbfc26dd27b6bdd967a2e646890ec504
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-08 17:49:29 +0800

    前台展示系统-热门推荐-列表页|详情页使用动效

---
+++ pro_htmlbase/Program/fashionShop/frontend/js/commonHotAdvise.js
@@ -0,0 +1,24 @@
+/* @by chuanmingxie */
+var hotGoods={
+	name:"热门推荐",
+	srcPath:"images/shopshow",
+	data:[
+		{href:"#",src:"s1.jpg",price:56.00},
+		{href:"#",src:"s2.jpg",price:97.00},
+		{href:"#",src:"s3.jpg",price:56.00},
+		{href:"#",src:"s4.jpg",price:94.00},
+		{href:"#",src:"s5.jpg",price:56.00},
+		{href:"#",src:"s6.jpg",price:56.00},
+		{href:"#",src:"s7.jpg",price:57.00},
+		{href:"#",src:"s8.jpg",price:56.00},
+		{href:"#",src:"s9.jpg",price:32.00},
+		{href:"#",src:"yifu1.jpg",price:56.00},
+		{href:"#",src:"yifu2.jpg",price:56.00},
+		{href:"#",src:"yifu3.jpg",price:76.00},
+		{href:"#",src:"yifu4.jpg",price:36.00},
+		{href:"#",src:"yifu5.jpg",price:196.00}]
+};
+
+function showHotGoods(){
+	
+}
