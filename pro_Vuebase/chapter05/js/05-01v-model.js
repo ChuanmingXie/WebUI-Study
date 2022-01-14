33f6b5ae2a207d5fd4cce4ee0b218f66f2d305fd
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-14 20:54:51 +0800

    5-01v-model-基本用法

--- pro_Vuebase/chapter05/js/05-01v-model.js
+++ pro_Vuebase/chapter05/js/05-01v-model.js
@@ -2,7 +2,25 @@
 var app=new Vue({
 	el:'#app',
 	data:{
-		message:''
-		tetContent:''
+		message:'',
+		txtContent:'',
+		inputMessage:'',
+		picked:true,
+		pickedG:'js',
+		checked:false,
+		checkedG:[],
+		selected:'js',
+		selectedM:[],
+		selectedvfor:'html',
+		options:[
+			{text:'HTML',value:'html'},
+			{text:'JavaScript',value:'js'},
+			{text:'CSS',value:'css'}
+		]
+	},
+	methods:{
+		handleInput:function(e){
+			this.inputMessage=e.target.value;
+		}
 	}
 })
