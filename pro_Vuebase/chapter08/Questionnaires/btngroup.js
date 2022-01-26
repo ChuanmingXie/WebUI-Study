971f61c3d1b3de5a1d17c53a6acc364e93ccd4bf
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-26 09:59:45 +0800

    8-3综合复习小应用-问卷调查-Vue.js-组件

--- pro_Vuebase/chapter08/Questionnaires/btngroup.js
+++ pro_Vuebase/chapter08/Questionnaires/btngroup.js
@@ -1,4 +1,28 @@
 /* 8-3综合复习小应用-问卷调查 */
 Vue.component('btngroup',{
-	
+	props:{
+		fontcolor:{
+			type:String,
+			default:"#000"
+		},
+		banned:{
+			type:Boolean,
+			default:true
+		}
+	},
+	template:'\
+		<button class="mybutton" @click="handleClick" :disabled="banned">\
+			<slot></slot>\
+		</button>',
+	methods:{
+		getStyle:function(){
+			return {
+				color:this.fontcolor,
+				":active color":'#fff'
+			}
+		},
+		handleClick:function(){
+			this.$emit('click');
+		}
+	}
 })
