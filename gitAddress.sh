f6aee6b9900c0898d898ba0af3917f53a53c3204
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-26 17:26:28 +0800

    修改

--- gitAddress.sh
+++ gitAddress.sh
@@ -7,12 +7,12 @@ OLD_EMAIL2="Chuanmingxie@outlook.com"
 OLD_EMAIL3="75523225+ChuanmingXie@users.noreply.github.com"
 CORRECT_NAME="ChuanmingXie"
 CORRECT_EMAIL="ChuanmingXie@outlook.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL3" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL2" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL3" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
