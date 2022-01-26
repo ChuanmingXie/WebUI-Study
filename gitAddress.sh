7668fd526435e356b89fe2ad66856f6ea25b135f
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-26 15:11:03 +0800

    修改用户名和邮箱设置

--- gitAddress.sh
+++ gitAddress.sh
@@ -1,26 +1,18 @@
-#修改设置
+#修改用户名和邮箱设置
 #!/bin/sh
 
-git filter-repo --env-filter '
-
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
-
-if [ "$GIT_COMMITTER_EMAIL" = "[ChuanmingXie@outlook.com]" ]
+git filter-branch --env-filter '
+OLD_EMAIL="ChuanmingXie@outlook.com"
+CORRECT_NAME="ChuanmingXie@outlook.com"
+CORRECT_EMAIL="ChuanmingXie@outlook.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    cn="[ChuanmingXie]"
-    cm="[ChuanmingXie@outlook.com]"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "[ChuanmingXie@outlook.com]" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    an="[ChuanmingXie]"
-    am="[ChuanmingXie@outlook.com]"
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+' --tag-name-filter cat -- --branches --tags
