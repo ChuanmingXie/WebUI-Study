ed253b1a9df2d731495c5ef8aebac0fb6130928c
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-26 17:19:07 +0800

    修改

--- gitAddress.sh
+++ gitAddress.sh
@@ -3,15 +3,17 @@
 
 git filter-branch --env-filter '
 OLD_EMAIL1="“chuanmingxie@outlook.com”"
-OLD_EMAIL2=Chuanmingxie@outlook.com
-OLD_EMAIL3=75523225+ChuanmingXie@users.noreply.github.com
+OLD_EMAIL2="Chuanmingxie@outlook.com"
+OLD_EMAIL3="75523225+ChuanmingXie@users.noreply.github.com"
 CORRECT_NAME="ChuanmingXie"
 CORRECT_EMAIL="ChuanmingXie@outlook.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" -o "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL3"];then
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" ]
+then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" -o "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL3"];then
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL2" ]
+then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
