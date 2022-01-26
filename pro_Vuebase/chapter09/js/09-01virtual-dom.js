f1a0e3c24bfd04d1c1ace11ea7b25bd2df753f4d
ChuanmingXie <ChuanmingXie@outlook.com>
2022-01-26 14:31:53 +0800

    9-1Virtual DOM简介-接口VNode

--- pro_Vuebase/chapter09/js/09-01virtual-dom.js
+++ pro_Vuebase/chapter09/js/09-01virtual-dom.js
@@ -1,10 +1,24 @@
 /* 9-1Virtual DOM简介 */
 // VNode 定义
-export interface VNode{
-	
+export interface VNode {
+	tag ? : string;			//当前节点的标签名
+	data ? : VNodeData;		//当前节点的数据对象
+	children ? : VNode[];
+	text ? : string;
+	elm ? : Node;
+	ns ? : string;
+	context ? : Vue;
+	key ? : string | number;
+	componentOptions: VNodeComponentOptions;
+	componentInstance ? : Vue;
+	parent ? : VNode;
+	raw ? : boolean;
+	isStatic ? : boolean;
+	isRootInsert ? : boolean;
+	isComment: boolean;
 }
 
 // VNodeData 定义
-export interface VNodeData{
-	
+export interface VNodeData {
+
 }
