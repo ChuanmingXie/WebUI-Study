1822f964cf7056da51fd24140782df3aca8bce87
ChuanmingXie <ChuanmingXie@outlook.com>
2022-04-09 18:26:09 +0800

    Bootstroop3-Site-BluePrint-全书案例

---
+++ pro_Bootstrap/bootstrap_blueprint/Chapter03/cloudwhales-themes/functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Roots includes
+ *
+ * The $roots_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ *
+ * Please note that missing files will produce a fatal error.
+ *
+ * @link https://github.com/roots/roots/pull/1042
+ */
+$roots_includes = array(
+  'lib/utils.php',           // Utility functions
+  'lib/init.php',            // Initial theme setup and constants
+  'lib/wrapper.php',         // Theme wrapper class
+  'lib/sidebar.php',         // Sidebar class
+  'lib/config.php',          // Configuration
+  'lib/activation.php',      // Theme activation
+  'lib/titles.php',          // Page titles
+  'lib/nav.php',             // Custom nav modifications
+  'lib/gallery.php',         // Custom [gallery] modifications
+  'lib/scripts.php',         // Scripts and stylesheets
+  'lib/extras.php',          // Custom functions
+);
+
+foreach ($roots_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'roots'), $file), E_USER_ERROR);
+  }
+
+  require_once $filepath;
+}
+unset($file, $filepath);
+//Wordpress 5.0+ 禁用 Gutenberg 编辑器
+add_filter('use_block_editor_for_post', '__return_false');
+remove_action( 'wp_enqueue_scripts', 'wp_common_block_scripts_and_styles' );
