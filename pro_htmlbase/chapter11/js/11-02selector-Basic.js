67d0701659febabecfb5cfcdfce28d470b2535b2
ChuanmingXie <ChuanmingXie@outlook.com>
2023-03-30 12:02:33 +0800

    WebUI-v1.0 -版本清除

--- pro_htmlbase/chapter11/js/11-02selector-Basic.js
+++
@@ -1,8 +0,0 @@
-/* 11-2jQuery选择器-基本选择器 */
-$(function(e){
-	$('#idDiv').css('color','bule');
-	$('.classDiv').css('background-color','#dddddd');
-	$('span').css('background-color','gray').css('color','white');
-	$("*").css('font-size','20px');
-	$('#iddiv,.classSpan').css('font-style','italic');
-});
